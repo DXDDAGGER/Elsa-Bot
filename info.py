791b4e67287a2b963e83278946ee4b1c0fd0162a
DXDDAGGER <sreeharipradeep38@gmail.com>
2025-07-17 13:59:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+BOT_TOKEN = environ['7647981293:AAEJfc81eJtHSlvSVGEmTg9ocpu6kygDNl8']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
